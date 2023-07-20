8adafc9b647f6bc7923a57ec20e1ee594e960c98
Fukuro192 <44007985+Fukuro192@users.noreply.github.com>
2023-07-20 20:35:22 +0200

    Create moroccoan-flag.js

---
+++ moroccoan-flag.js
@@ -0,0 +1,15 @@
+{
+    "contact": "discord @yegorenji",
+    "templates": [
+        {
+            "name": "Moroccan Flag",
+            "sources": [
+                "https://cdn.discordapp.com/attachments/1131621221864321025/1131655312647139441/236f086ea6c86673590e704ce109fe6bb.png"
+            ],
+            "x": -261,
+            "y": 446
+        }
+    ],
+    "whitelist": [],
+    "blacklist": []
+}
