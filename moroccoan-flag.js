81e62afd6fc6fc835c7e1bffb6d4365779cdde31
Fukuro192 <salah8elmes.2000@gmail.com>
2023-07-20 22:01:27 +0200

    update

--- moroccoan-flag.js
+++ moroccoan-flag.js
@@ -1,15 +1,15 @@
 {
-    "contact": "discord @yegorenji",
+    "contact": "",
     "templates": [
-        {
-            "name": "Moroccan Flag",
-            "sources": [
-                "https://cdn.discordapp.com/attachments/1131621221864321025/1131655312647139441/236f086ea6c86673590e704ce109fe6bb.png"
-            ],
-            "x": -261,
-            "y": 446
-        }
+      {
+        "name": "flag alternate spot",
+        "sources": [
+          "https://cdn.discordapp.com/attachments/1131633763038793851/1131675197896925366/small_flag.png"
+        ],
+        "x": 634,
+        "y": 809
+      }
     ],
     "whitelist": [],
     "blacklist": []
-}
+  }
